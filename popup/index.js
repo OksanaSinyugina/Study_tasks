a5ed2f39fb14c7009faed5516b60c724dfc66864
OksanaSinyugina <111419697+OksanaSinyugina@users.noreply.github.com>
2022-09-01 18:01:48 +0300

    updates

--- popup/index.js
+++ popup/index.js
@@ -1,3 +1,23 @@
-const modal = $.modal()
+const modal = $.modal({
+    title: 'Popup window',
+    closable: true,
+    content: `
+        <p>Hey.</p>
+        <p>Lorem ipsum dolor sit.</p>
+        <p>Lorem ipsum dolor sit.</p>
+        <p>Lorem ipsum dolor sit.</p>
+    `,
+    width: '200px',
+    footerButtons: [
+        {text: 'OK', type: 'primary', handler() {
+            console.log('Primary btn clicked')
+            modal.close()
+         }},
+        {text: 'CANCEL', type: 'danger', handler() {
+            console.log('Danger btn clicked')
+            modal.close()
+         }},
+    ]
+})
 
-// myModal.open()
+modal.open()
